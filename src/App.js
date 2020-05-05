d0f153902f123a3d16fafafeaaf30df1885086a8
Computrailer <letsgood56@gmail.com>
2020-05-05 10:09:22 -0300

    created_fields_of_form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './css/global.css';
+import './css/App.css';
+import './css/Sidebar.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label><br/>
+            <input name="github_username" id="github_username" required/><br/>
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="courses" >Cursos</label><br/>
+            <input name="courses" id="courses" required></input>
+          </div>
+          <div className="inline">
+            <div>
+              <label htmlFor="latitude">Latitude</label><br/>
+              <input name="latitude" id="latitude" required></input><br/>
+            </div>
+            <div>
+              <label htmlFor="longitude" >Longitude</label><br/>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+          
+          <button>Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
     </div>
   );
 }
